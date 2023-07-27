d18dc74192138c126ec5a9a2135605c7cd8ead84
pth1209 <113861167+pth1209@users.noreply.github.com>
2023-07-27 07:09:23 -0500

    update frontend

--- src/components/Summarizer.js
+++ src/components/Summarizer.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Summarizer = () => {
+  const [article, setArticle] = useState({
+    "url": "",
+    "summary":  ""
+  });
+
+  const handleSubmit = () => {
+    alert("Submitted!")
+  }
+
   return (
-    <div>
-      Summarizer
-    </div>
+    <section className = "mt-16 w-full max-w-xl">
+      <div className = "flex flex-col w-full gap-2">
+        <form 
+        className = "relative flex justify-center items-center"
+        onSubmit = {handleSubmit}>
+            <input 
+              type = "url"
+              placeholder = "Enter a URL to summarize!"
+              defaultValue=""
+              onChange = {(e) => {setArticle({...article, url: e.target.value})}}
+              required
+              className = "input peer">
+            </input>
+            <button type = "submit" className = "submit_button">Click me!</button>
+        </form>
+        {/* Display browing history */}
+      </div>
+        {/* Display results of the summarizer */}
+    </section>
   )
 }
 
